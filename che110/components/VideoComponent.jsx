00325e8d733bb74337d4a5b6960c21b7c1bdafb7
Sachin <66558571+SachinD6@users.noreply.github.com>
2023-04-06 02:36:20 +0530

    Update VideoComponent.jsx

--- che110/components/VideoComponent.jsx
+++ che110/components/VideoComponent.jsx
@@ -7,7 +7,7 @@ export default function VideoComponent() {
       <div className="bg-video h-screen max-w-screen object-cover object-center relative">
         <div className="absolute inset-0 max-w-7xl mx-auto">
           <div className="relative z-20 "><Navbar /></div>
-          <div className="absolute z-30"><h1 className="text-7xl font-semibold text-white mx-auto items-center">It's time to take climate action
+          <div className="absolute z-30"><h1 className="text-7xl font-semibold text-white mx-auto items-center">Its time to take climate action
 </h1></div>
           <div className="absolute bottom-0 mx-auto text-white text-4xl font-bold p-4 rounded-lg z-10 grid grid-cols-3 grid-rows-2 gap-y-10 gap-x-20 mx-auto">
             <StatCard title="Carbon Dioxide" description="parts per million(current)" stat={419} isUp/>
